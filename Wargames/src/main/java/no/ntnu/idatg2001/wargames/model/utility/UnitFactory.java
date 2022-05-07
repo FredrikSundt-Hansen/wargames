ab38f300e96cacd077b525d29889d687c6ab03b7
fredsu <fredsufredsu@stud.ntnu.no>
2022-05-07 18:03:39 +0200

    Created UnitFactory class as a Singleton. Two methods, one for creating a single Unit and one to make multiple units.

---
+++ Wargames/src/main/java/no/ntnu/idatg2001/wargames/model/utility/UnitFactory.java
@@ -0,0 +1,79 @@
+package no.ntnu.idatg2001.wargames.model.utility;
+
+import java.util.ArrayList;
+import java.util.List;
+import no.ntnu.idatg2001.wargames.model.units.CavalryUnit;
+import no.ntnu.idatg2001.wargames.model.units.CommanderUnit;
+import no.ntnu.idatg2001.wargames.model.units.InfantryUnit;
+import no.ntnu.idatg2001.wargames.model.units.RangedUnit;
+import no.ntnu.idatg2001.wargames.model.units.Unit;
+
+/**
+ * Factory class, used to make units without exposing creation logic to user.
+ */
+public class UnitFactory {
+  private static volatile UnitFactory instance;
+
+  /**
+   * Private constructor restricting object creation,
+   * making constructor only available when used with getInstance.
+   */
+  private UnitFactory() {}
+
+  /**
+   * Static method to get an instance of the class. Singleton design.
+   *
+   * @return An instance of UnitFactory.
+   */
+  public static UnitFactory getInstance() {
+    if (instance == null) {
+      synchronized (UnitFactory.class) {
+        instance = new UnitFactory();
+      }
+    }
+    return instance;
+  }
+
+  /**
+   * Method to create a single Unit, based on unitType input.
+   * Passes the name and health to the new object.
+   *
+   * @param unitType String, the unit type to create as a string.
+   *                 "Infantry", "Ranged", "Cavalry" or "Commander" for the different units.
+   * @param name The name of the new unit.
+   * @param health The health of the new unit.
+   * @return An instance of the new unit.
+   */
+  public Unit createUnit(String unitType, String name, int health) {
+    switch (unitType) {
+      case "Infantry":
+        return new InfantryUnit(name, health);
+      case "Cavalry":
+        return new CavalryUnit(name, health);
+      case "Ranged":
+        return new RangedUnit(name, health);
+      case "Commander":
+        return new CommanderUnit(name, health);
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * Method to create n many units. Creates units based on unitType input.
+   *
+   * @param unitType String, the unit type to create as a string. "Infantry", "Ranged", "Cavalry" or
+   *     "Commander" for the different units.
+   * @param name The name of the new unit.
+   * @param health The health of the new unit.
+   * @param numberUnits How many units to create of that type.
+   * @return List of units, with the given health, name, and number.
+   */
+  public List<Unit> createMultipleUnits(String unitType, String name, int health, int numberUnits) {
+    List<Unit> unitList = new ArrayList<>();
+    for (int i = 0; i < numberUnits; i++) {
+      unitList.add(createUnit(unitType, name, health));
+    }
+    return unitList;
+  }
+}
