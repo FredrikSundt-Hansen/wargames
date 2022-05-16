4dde64bcd8cf54832d4720bb4516bfaf21c90814
fredsu <fredsufredsu@stud.ntnu.no>
2022-05-16 13:37:23 +0200

    Wrapped imageview and button in pane instead of an anchor-pane.

--- Wargames/src/main/java/no/ntnu/idatg2001/wargames/ui/controllers/MainWindowController.java
+++ Wargames/src/main/java/no/ntnu/idatg2001/wargames/ui/controllers/MainWindowController.java
@@ -1,13 +1,31 @@
 package no.ntnu.idatg2001.wargames.ui.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import no.ntnu.idatg2001.wargames.ui.views.WargamesApplication;
 
 public class MainWindowController implements Initializable {
+  @FXML
+  private Button buttonNewBattle;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+
+  public void onMouseDragOverLogo(MouseEvent mouseDragEvent) {
+
+  }
+
+  public void onNewBattleButtonClick(MouseEvent mouseEvent) throws IOException {
+    WargamesApplication.goToBattleMaker();
 
   }
 }
